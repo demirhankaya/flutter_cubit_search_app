8a9ab4bf8a647d31466a3b4df03920bb4846e23a
demirhankaya <48810452+demirhankaya@users.noreply.github.com>
2024-10-11 17:32:35 +0300

    Localization için gerekli yapı kuruldu. İstenilen metinler app_tr.arb içerisine girilecek

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit_search_app/core/extensions/l10n_x.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,12 +9,18 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      /// [L10n]
+      /// Delegate: Temsilci Listesi
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+
+      /// Desteklenen Diller
+      supportedLocales: AppLocalizations.supportedLocales,
+      title: "Search App",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: "Search App"),
     );
   }
 }
@@ -40,14 +48,14 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(context.translate.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              context.translate.title,
             ),
             Text(
               '$_counter',
